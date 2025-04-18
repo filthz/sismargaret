caf56e2ef627acd45b43f0e42cec5952bc5f9876
NyanCatTW1 <17372086+NyanCatTW1@users.noreply.github.com>
2025-04-18 16:34:03 +0800

    Make start-miner.sh work when called from a different dir

--- start-miner.sh
+++ start-miner.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 sudo docker stop $(sudo docker ps -aq -f name=sismargaret-miner)
 sudo docker rm $(sudo docker ps -aq -f name=sismargaret-miner)
-sudo docker run --init -it -v $(pwd)/logs:/logs -v $(pwd)/data:/tmp/dreadpool -p 7777:7777 -p 24242:24242 --name sismargaret-miner -d --restart unless-stopped sismargaret-miner
+
+# Autodetect miner directory
+MINER_DIR=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+sudo docker run --init -it -v ${MINER_DIR}/logs:/logs -v ${MINER_DIR}/data:/tmp/dreadpool -p 7777:7777 -p 24242:24242 --name sismargaret-miner -d --restart unless-stopped sismargaret-miner
 
 echo "To view miner log, check the logs folder or run this command:"
 echo 'sudo docker logs -f $(sudo docker ps -aq -f name=sismargaret-miner)'
